0d4dc2955d553bd48346a48c5b41f79ab21047ed
NickkTMD <nickk@havocmc.net>
2025-01-20 18:38:35 -0800

    Astro

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+// import partytown from "@astrojs/partytown";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    mdx(),
+    // partytown({
+    //   config: {
+    //     forward: ["dataLayer.push"],
+    //   },
+    // }),
+  ],
+  image: {
+    service: { entrypoint: "astro/assets/services/sharp" },
+  },
+});
